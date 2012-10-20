20a83d7e667763fcb7aa845c74afd6d8b480fd17
Josh Mervine <jmervine@yp.com>
2012-10-19 17:24:43 -0700

    initial commit

---
+++ listener.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+require 'yaml'
+
+config = YAML.load_file("./config.yml")
+
+error 403 do
+  'Access forbidden!'
+end
+
+before do
+  403 if ENV['RACK_ENV'] == "production" and not config['github_ip_list'].include?(request.ip)
+end
+
+get '/' do
+  raise "application root directory not found" unless File.directory?(config['application_root'])
+  %x{ set -x; cd #{ config['application_root'] } && git pull }
+end
