6a8f9b8c0aa47915e6059cecc478fd577f65c028
Josh Mervine <jmervine@yp.com>
2012-10-19 17:47:53 -0700

    fixing for rackup

---
+++ config.ru
@@ -0,0 +1,2 @@
+require './listener'
+run Listener
--- listener.rb
+++ listener.rb
@@ -1,17 +1,19 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'yaml'
 
-config = YAML.load_file("./config.yml")
+class Listener < Sinatra::Base
+  @@config = YAML.load_file("./config.yml")
 
-error 403 do
-  'Access forbidden!'
-end
+  error 403 do
+    'Access forbidden!'
+  end
 
-before do
-  403 if ENV['RACK_ENV'] == "production" and not config['github_ip_list'].include?(request.ip)
-end
+  before do
+    403 if ENV['RACK_ENV'] == "production" and not config['github_ip_list'].include?(request.ip)
+  end
 
-get '/' do
-  raise "application root directory not found" unless File.directory?(config['application_root'])
-  %x{ set -x; cd #{ config['application_root'] } && git pull }
+  get '/' do
+    raise "application root directory not found" unless File.directory?(config['application_root'])
+    %x{ set -x; cd #{ config['application_root'] } && git pull }
+  end
 end
