723a28c2bdce467ebd97fba15be64a7f61d3ee9f
Joshua P. Mervine <joshua@mervine.net>
2012-10-21 01:35:27 -0400

    it's working now

--- listener.rb
+++ listener.rb
@@ -1,19 +1,21 @@
 require 'sinatra/base'
 require 'yaml'
+require 'pp'
 
 class Listener < Sinatra::Base
   @@config = YAML.load_file("./config/config.yml")
 
-  error 403 do
-    'Access forbidden!'
+  error do
+    'ACK!'
   end
 
-  get '/pull' do
-    403 if ENV['RACK_ENV'] == "production" and not @@config['github_ip_list'].include?(request.ip)
+  post '/pull' do
+
+    raise "forbidden" unless @@config['github_ip_list'].include?(request.ip)
 
     command = [ "cd #{@@config['application_root']}" ]
     command.push @@config['before_cmd'] if @@config['before_cmd']
-    command.push "git push"
+    command.push "git pull"
     command.push @@config['after_cmd'] if @@config['after_cmd']
 
     raise "application root directory not found" unless File.directory?(@@config['application_root'])
